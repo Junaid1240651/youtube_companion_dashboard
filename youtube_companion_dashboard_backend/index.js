a7b62f3d5cfdadd3a77753233a54ae78c6d42e46
SDE1Junaid <junaid@akratech.in>
2025-07-21 01:00:10 +0530

    log add to debug backend auth

--- youtube_companion_dashboard_backend/index.js
+++ youtube_companion_dashboard_backend/index.js
@@ -27,6 +27,7 @@ const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_SECRET,
   process.env.GOOGLE_REDIRECT_URI
 );
+console.log(oauth2Client);
 
 const OAUTH_SCOPES = [
   'https://www.googleapis.com/auth/youtube.force-ssl',
